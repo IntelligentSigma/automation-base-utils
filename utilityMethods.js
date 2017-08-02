048541e18a7698c9fdb8343089a45d5167e0aee7
MamanakisDM <mamanakisdm@familysearch.org>
2017-08-02 16:40:12 -0600

    Update to the utility file and properties.json

--- utilityMethods.js
+++ utilityMethods.js
@@ -85,7 +85,130 @@ var utilityMethods = function(){
         return elem.isDisplayed();
       })
       .first();
-  }
+  };
+
+  utilityMethods.prototype.wait = function (waitFunction, timeouts, timeoutMessage) {
+    var self = this;
+    var timeout = timeouts ? timeouts : browser.testEnv.waitTimeout;
+
+    return browser.wait(waitFunction, timeout, timeoutMessage).then(function(pass){return pass}, function(error){
+      //retry if the error is a stale element, half the timeout
+      if(error.name == "StaleElementReferenceError") {
+        return self.wait(waitFunction, timeout/2, timeoutMessage);
+      } else {
+        throw error;
+      }
+    });
+  };
+
+  utilityMethods.prototype.click = function (getElementFunction, timeouts, elementDescription, tries) {
+    var self = this;
+    var timeout = timeouts ? timeouts : browser.testEnv.waitTimeout;
+
+    return this.wait(until.visibilityOf(getElementFunction), timeout, elementDescription + " should be visible before click").then(function () {
+      return getElementFunction.click().then(function(pass){return pass}, function(error){
+        //try clicking 3 times before throwing ans error
+        if (tries) {
+          tries = tries + 1;
+        } else {
+          tries = 0
+        }
+
+        if(3 < tries) {
+          console.log("retry click error was: " + error);
+          return self.click(getElementFunction, timeout, elementDescription, tries);
+        } else {
+          throw error;
+        }
+      });
+    });
+  };
+
+  utilityMethods.prototype.isDisplayedWithRetry = function retryIsDisplayedForStaleElement(el, timeouts, retries, elementDescription) {
+    var timeout = timeouts ? timeouts : browser.testEnv.waitTimeout;
+
+    return browser.wait(function () {
+      return el.isDisplayed().then(function (displayed) {
+        return !!displayed;
+      }, function (err) {
+        if (retries > 0) {
+          console.log("Retrying " + elementDescription);
+          return retryIsDisplayedForStaleElement(el, timeout, retries - 1, elementDescription);
+        } else {
+          throw "error displaying " + elementDescription + " after 3 retries";
+        }
+      });
+    }, timeout, elementDescription + " not displayed !!!");
+  };
+
+  utilityMethods.prototype.isDisplayedWithRefresh = function refreshIsDisplayed(el, timeouts, retries, elementDescription) {
+    var timeout = timeouts ? timeouts : browser.testEnv.waitTimeout;
+
+    return browser.wait(function () {
+      return el.isDisplayed().then(function (displayed) {
+        return !!displayed;
+      }, function (err) {
+        if (retries > 0) {
+          console.log("Retrying " + elementDescription);
+          browser.refresh();
+          browser.sleep(5000);
+          return refreshIsDisplayed(el, timeout, retries - 1, elementDescription);
+        } else {
+          throw "error displaying " + elementDescription + " after 3 retries";
+        }
+      });
+    }, timeout, elementDescription + " not displayed !!!");
+  };
+
+  utilityMethods.prototype.waitForPromiseTest = function (getElementFunction, testFn, timeouts, errorDescription) {
+    var timeout = timeouts ? timeouts : browser.testEnv.waitTimeout;
+
+    browser.wait(function () {
+      var deferred = q.defer();
+      getElementFunction().then(function (data) {
+        deferred.resolve(testFn(data));
+      });
+      return deferred.promise;
+    }, timeout, errorDescription);
+  };
+
+  /**
+   * This method helps determine when an element enters or leaves the dom. It helsp avoid the Stale Element reference
+   * exception, by first getting the body which is not changing on the page. (unless you are reloading the entire page)
+   *
+   * @param locator - protractor Locater within the '<body>' tag (example:  By.css['.my-selector)]
+   * @param present - whether element is to become present or not present
+   * @param timeout -  global timeout
+   * @param errorDescription - error description text
+   */
+  utilityMethods.prototype.waitForUnstableElement = function (locator, present, timeouts, errorDescription) {
+    var timeout = timeouts ? timeouts : browser.testEnv.waitTimeout;
+
+    browser.wait(function () {
+      return element(By.css('body')).isElementPresent(locator)
+          .then(function (isPresent) {
+            return (isPresent === present);
+          });
+    }, timeout, errorDescription);
+  };
+
+  /**
+   *
+   * @param conditionFunc boolean function (example: function () {return typeof item !== "undefined" && typeof item2 !== "undefined"})
+   * @param execFunc what to execute when condition is true (example: Done) (example: function() {return true;}) (example: function() {variable = somthingnew()})
+   * @param interval how long to wait (ms) before retrying condition
+   */
+
+  utilityMethods.prototype.when = function(conditionFunc, execFunc, interval) {when(conditionFunc, execFunc, interval)};
+
+  var when = function (conditionFunc, execFunc, interval){
+    if (conditionFunc()){
+      execFunc();
+    }else{
+      setTimeout(function(){when(conditionFunc, execFunc, interval);}, interval);
+    }
+  };
+
 };
 
 module.exports = utilityMethods;
